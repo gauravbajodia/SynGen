da180250ca746736d91fee3e94835d80c2c0f1ad
madscientist <58301570+gauravbajodia@users.noreply.github.com>
2021-06-08 23:42:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ obj = generate_from_scratch()
 
 app = Flask(__name__)
 
+if __name__=="__main__":
+    app.run(host=os.getenv('IP', '0.0.0.0'), 
+            port=int(os.getenv('PORT', 4444)))
+
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 app.secret_key = '1234'
